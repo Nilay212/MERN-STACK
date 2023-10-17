607c6b97c528bca745c46b3e0fffa21a48bbb3ec
Nilay <pandaynilay@gmail.com>
2023-10-17 11:44:23 +0530

    component added

--- E-Commerce/src/main.jsx
+++ E-Commerce/src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Register from "./pages/Register";
+import Privacy from "./pages/Privacy";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ E-Commerce/src/pages/Privacy.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <>
+      <h1>Privacy Policy</h1>
+    </>
+  );
+};
+
+export default Privacy;
